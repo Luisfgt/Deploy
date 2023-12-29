2bc3b420205a6ca73291c07e1badeeb4fd380d42
Luisfgt <luisfgonzalezt09@gmail.com>
2023-12-29 15:56:35 -0500

    luis2

--- clientVite/client/src/components/Modals/ModalCreateVideoGame.jsx
+++ clientVite/client/src/components/Modals/ModalCreateVideoGame.jsx
@@ -5,7 +5,7 @@ import Select from "react-select"
 import Card from "../Home/Card/Card"
 import { getAllVideoGames, setFilterGames } from "../../redux/reducers/videogameSlice"
 import validate from "./validateInfo"
-axios.defaults.baseURL = 'https://deploy-production-3bca.up.railway.app/'
+axios.defaults.baseURL = 'https://deploy-production-3bca.up.railway.app'
 const URL_CREATE = '/videogames'
 const URL_GET_ALL = '/videogames'
 
